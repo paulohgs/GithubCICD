f418a8ba68080aaab3919d698993b4a41ea529c2
Paulo Henrique <phsilva098@gmail.com>
2023-08-09 09:08:06 -0300

    Initial Commit

---
+++ GithubCICD/GithubCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubCICDApp.swift
+//  GithubCICD
+//
+//  Created by Paulo Henrique Gomes da Silva on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
